aa01eda4cbf57d850a7cb1024e911700670d69d1
Vinicius Trajano <viniciustrajano46@gmail.com>
2026-08-26 04:55:48 -0300

    Entidades feitas

--- src/main/java/br/com/trajano_trajano/database/entities/Comment.java
+++ src/main/java/br/com/trajano_trajano/database/entities/Comment.java
@@ -1,4 +1,24 @@
 package br.com.trajano_trajano.database.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NegativeOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_comments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+
 }
